2b35772e9e6750012fc48dd7306fcb0e8ed4a1f1
SAFIA MAHER AL QADI <145432448+safiyaalqadi@users.noreply.github.com>
2026-03-31 15:34:26 +0300

    frontendPush

---
+++ TaskManagerProj/TaskManagerProj/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagerProj.Entitys;
+
+namespace TaskManagerProj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+           private readonly UserManager<UserEntity> _userManager;
+           
+        }
+    }
+}
